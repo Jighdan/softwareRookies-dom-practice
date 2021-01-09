615d0c65a14cf5c586d8034536d9e300eca8243c
Reinny <reynsth@gmail.com>
2021-01-09 15:09:02 -0400

    :bug: New rows start with a clean value

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -8,7 +8,7 @@ export default {
 		// Check if there are headers in the table and adds the to the row
 		if (state.table.columns) {
 			Object.keys(state.table.columns).forEach(columnName => {
-				rowTemplate.cells[columnName] = state.table.columns[columnName]
+				rowTemplate.cells[columnName] = "";
 			});
 		};
 

296bdd8f389864151f154bc176a77da774bcd1b4
Reinny <reynsth@gmail.com>
2021-01-13 08:36:16 -0400

    :truck: Renaming variables

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -4,14 +4,11 @@ import tableRow from "./tableRow";
 import tableRowSelector from "./tableRowSelector";
 import { capitalizeString } from "../../plugins/textFormat";
 
-class Table extends ComponentBase {
+export default class Table extends ComponentBase {
 	constructor () {
-		super({ store, element: document.getElementById("tableContainer") })
-		this.tableData = store.state.table;
+		super({ store, element: document.getElementById("table") })
 		this.tableHeader = null;
 		this.tableBody = null;
-
-		this.element.setAttribute("id", "tableContainer")
 	};
 
 	generateHeader() {
@@ -20,7 +17,7 @@ class Table extends ComponentBase {
 
 		const tableHeadEmptyCell = document.createElement("th");
 		
-		const tableHeaderCells = Object.keys(this.tableData.columns).map(columnName => {
+		const tableHeaderCells = Object.keys(store.state.table.columns).map(columnName => {
 			const tableHeaderCellContent = document.createElement("h3");
 			tableHeaderCellContent.innerText = capitalizeString(columnName);
 
@@ -43,7 +40,7 @@ class Table extends ComponentBase {
 	};
 
 	generateBody() {
-		const tableBodyRows = this.tableData.rows.map(row => tableRow(row, this.tableData.columns));
+		const tableBodyRows = store.state.table.rows.map(row => tableRow(row, store.state.table.columns));
 		const tableBody = document.createElement("tbody");
 		tableBodyRows.forEach(row => tableBody.appendChild(row));
 		this.element.appendChild(tableBody);
@@ -51,15 +48,15 @@ class Table extends ComponentBase {
 	};
 
 	render() {
-		// Clear the table if it has a header or body
-		if (this.tableHead || this.tableBody) {
-			this.element.innerHTML = "";
-		}
+		if (Object.keys(store.state.table.columns).length && store.state.table.rows.length) {
+			// Clear the table if it has a header or body
+			if (this.tableHead || this.tableBody) {
+				this.element.innerHTML = "";
+			}
 
-		this.generateHeader();
-		this.generateBody();
-		return this.element;
-	}
+			this.generateHeader();
+			this.generateBody();
+			return this.element;
+		};
+	};
 };
-
-export default Table;
